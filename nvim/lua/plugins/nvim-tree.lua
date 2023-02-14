d8b315ad627180c04c287334944b3e5bfdca7ebe
minocrafft <minocrafft@gmail.com>
2023-02-14 16:58:25 +0900

    change nvim_tree setup (deprecated open_on_setup & open_on_setup_file)

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -4,18 +4,36 @@ if not status then
     return
 end
 
-local config_status, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status then
-    return
-end
+-- local config_status, nvim_tree_config = pcall(require, "nvim-tree.config")
+-- if not config_status then
+--     return
+-- end
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup({
+local function open_nvim_tree(data)
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+nvim_tree.setup {
     disable_netrw = true,
-    open_on_setup = true,
-    open_on_setup_file = true,
     diagnostics = {
         enable = true,
         icons = {
@@ -34,4 +52,6 @@ require("nvim-tree").setup({
         group_empty = true,
     },
     filters = { dotfiles = true },
-})
+}
+
+
