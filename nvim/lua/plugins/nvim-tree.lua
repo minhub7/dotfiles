fd7f11c7a7b5b2be22c5779a1bd344815055960b
minhub7 <minhub6@gmail.com>
2023-02-17 10:33:56 +0900

    nvim) deprecated open_on_setup

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -12,26 +12,6 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-local function open_nvim_tree(data)
-  -- buffer is a [No Name]
-  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
-
-  if not no_name and not directory then
-    return
-  end
-
-  -- change to the directory
-  if directory then
-    vim.cmd.cd(data.file)
-  end
-
-  -- open the tree
-  require("nvim-tree.api").tree.open()
-end
-
 nvim_tree.setup {
     disable_netrw = true,
     diagnostics = {
@@ -53,5 +33,4 @@ nvim_tree.setup {
     },
     filters = { dotfiles = true },
 }
-
 
