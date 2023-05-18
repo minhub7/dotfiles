4608014e5936d630b552be9281a01ebaa8db2d64
minhub7 <minhub6@gmail.com>
2023-05-18 23:41:56 +0900

    feat(nvim): add nvchad

---
+++ nvim.chad/lua/custom/configs/null-ls.lua
@@ -0,0 +1,32 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettierd.with {
+    filetypes = {
+      "html",
+      "markdown",
+      "css",
+      "json",
+      "yaml",
+      "toml",
+    }
+  }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
