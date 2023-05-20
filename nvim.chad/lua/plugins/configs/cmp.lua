85fd640d17e59f77bde6537aa7c8ab70a654c851
minhub7 <minhub6@gmail.com>
2023-05-20 14:42:46 +0900

    fix(nvim): fix mappings & completeopt

--- nvim.chad/lua/plugins/configs/cmp.lua
+++ nvim.chad/lua/plugins/configs/cmp.lua
@@ -46,7 +46,7 @@ end
 
 local options = {
   completion = {
-    completeopt = "menu,menuone",
+    completeopt = "menu,menuone,noselect",
   },
 
   window = {
@@ -69,8 +69,6 @@ local options = {
   formatting = formatting_style,
 
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
