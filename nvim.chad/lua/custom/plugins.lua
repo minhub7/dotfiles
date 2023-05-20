ad233d86e8d3baa02905508bb1c925d37a5f0692
minocrafft <minocrafft@gmail.com>
2023-05-20 15:21:30 +0900

    feat(nvim): add copilot

--- nvim.chad/lua/custom/plugins.lua
+++ nvim.chad/lua/custom/plugins.lua
@@ -72,6 +72,35 @@ local plugins = {
     end,
   },
 
+  -- Copilot
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "copilot", group_index = 2 },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path", group_index = 2 },
+      },
+    },
+  },
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
