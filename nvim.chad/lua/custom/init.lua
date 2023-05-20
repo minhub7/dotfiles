38906f98fad41df074a1e1280767828ad5598a75
minocrafft <minocrafft@gmail.com>
2023-05-20 14:41:21 +0900

    fix(nvim): fix conventions

--- nvim.chad/lua/custom/init.lua
+++ nvim.chad/lua/custom/init.lua
@@ -6,53 +6,52 @@
 --   command = "tabdo wincmd =",
 -- })
 
-
-vim.scriptencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
 vim.g.copilot_no_tab_map = true
 
 --------------------------------------------------
 local options = {
-    encoding='utf-8',
-    fileencoding='utf-8',
-    title=true,
-    backup=false,  -- don't save backup files
-    writebackup=false,
-    mouse="a",
-    laststatus=3,  -- global statusline
-    timeoutlen=400,
-    updatetime=250,  -- faster completion
+  encoding = "utf-8",
+  fileencoding = "utf-8",
+  title = true,
+  backup = false, -- don't save backup files
+  writebackup = false,
+  mouse = "a",
+  laststatus = 3, -- global statusline
+  timeoutlen = 400,
+  updatetime = 250, -- faster completion
 
-    -- line numbers
-    number=true,
-    relativenumber=true,
+  -- line numbers
+  number = true,
+  relativenumber = true,
 
-    -- tabs & indentation
-    tabstop=4,
-    shiftwidth=4,
-    smarttab=true,
-    expandtab=true,
-    autoindent=true,
-    smartindent=true,
+  -- tabs & indentation
+  tabstop = 4,
+  shiftwidth = 4,
+  smarttab = true,
+  expandtab = true,
+  autoindent = true,
+  smartindent = true,
 
-    -- cursor line
-    cursorline=true,
+  -- cursor line
+  cursorline = true,
 
-    -- appearance
-    termguicolors=true,
+  -- appearance
+  termguicolors = true,
 
-    -- backspace
-    backspace={'start', 'eol', 'indent'},
+  -- backspace
+  backspace = { "start", "eol", "indent" },
 
-    -- split windows
-    splitright=true,
-    splitbelow=true,
+  -- split windows
+  splitright = true,
+  splitbelow = true,
 
-    -- search settings
-    smartcase=true,
-    ignorecase=true,
-    hlsearch=true,
+  -- search settings
+  smartcase = true,
+  ignorecase = true,
+  hlsearch = true,
 }
 
 for k, v in pairs(options) do
-    vim.opt[k] = v
+  vim.opt[k] = v
 end
