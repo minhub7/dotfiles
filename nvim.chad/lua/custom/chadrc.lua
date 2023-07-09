5b4a7ae788c97e648904f8c73ab6b647c860fb1d
minocrafft <minocrafft@gmail.com>
2023-07-09 21:08:17 +0900

    nvim) change themes

--- nvim.chad/lua/custom/chadrc.lua
+++ nvim.chad/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "oxocarbon",
-  theme_toggle = { "oxocarbon", "github_dark" },
+  theme = "everforest",
+  theme_toggle = { "everforest", "github_dark" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
