81104a56cacecdd3026d5dba0303be60364e7e8c
minocrafft <minocrafft@gmail.com>
2023-07-19 15:49:19 +0900

    fix) add lua_ls to lspconfig

--- nvim.chad/lua/custom/configs/lspconfig.lua
+++ nvim.chad/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "pyright" }
+local servers = { "lua_ls", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -18,5 +18,14 @@ vim.diagnostic.config {
   underline = true,
 }
 
---
 -- lspconfig.pyright.setup { blabla}
+
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
