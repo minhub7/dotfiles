2b2273a5336134a221256c493b0327edbcf54021
minocrafft <minocrafft@gmail.com>
2023-08-01 15:57:53 +0900

    Fix statusline themes

--- nvim.chad/lua/custom/chadrc.lua
+++ nvim.chad/lua/custom/chadrc.lua
@@ -14,27 +14,7 @@ M.ui = {
   telescope = { style = "bordered" },
 
   statusline = {
-    overriden_modules = function()
-      local fn = vim.fn
-      return {
-        cursor_position = function()
-          -- default cursor_position module
-          local line = fn.line "."
-          local col = fn.virtcol "."
-          local total_line = fn.line "$"
-
-          local left_sep = "%#St_pos_sep#" .. "" .. "%#St_pos_icon#" .. " "
-          local chad = string.format(" %d:%d", line, col) .. " │ "
-          local text = math.modf((line / total_line) * 100) .. tostring "%%"
-
-          text = string.format("%4s", text)
-          text = (line == 1 and "Top") or text
-          text = (line == total_line and "Bot") or text
-
-          return left_sep .. "%#St_pos_text#" .. chad .. text .. " "
-        end,
-      }
-    end,
+    theme = "vscode_colored",
   },
 
   tabufline = {
