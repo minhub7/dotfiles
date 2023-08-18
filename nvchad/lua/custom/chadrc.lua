925d1e809565cf13a4d7c207fc8245a5c0e66974
minocrafft <minocrafft@gmail.com>
2023-08-18 09:00:32 +0900

    change default themes

--- nvchad/lua/custom/chadrc.lua
+++ nvchad/lua/custom/chadrc.lua
@@ -6,7 +6,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "chadracula",
-  theme_toggle = { "everforest", "chadracula" },
+  theme_toggle = { "chadracula", "everforest" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
