8458e124f5a6de331d805a01248cb4304f621c25
minocrafft <minocrafft@gmail.com>
2023-10-17 16:15:44 +0900

    remove submodule

---
+++ nvchad/custom/chadrc.lua
@@ -0,0 +1,34 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "chadracula",
+  theme_toggle = { "chadracula", "everforest" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+
+  telescope = { style = "bordered" },
+
+  statusline = {
+    theme = "vscode_colored",
+  },
+
+  tabufline = {
+    show_numbers = true,
+  },
+
+  nvdash = {
+    load_on_startup = true,
+  },
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
---
+++ nvchad/custom/configs/lspconfig.lua
@@ -0,0 +1,44 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "lua_ls", "pyright", "dartls", "rust_analyzer" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+vim.diagnostic.config {
+  virtual_text = false,
+  underline = true,
+}
+
+-- lspconfig.pyright.setup { blabla}
+
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
+
+lspconfig.rust_analyzer.setup {
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      -- checkOnSave = {
+      --   command = "clippy",
+      -- },
+    },
+  },
+}
